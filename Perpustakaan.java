9c1af639450ae2713309bb5f0e1bc4fd80e11598
zramzy59 <zramzy59@gmail.com>
2025-03-16 17:24:39 +0700

    Perpustakaan.java

--- Perpustakaan.java
+++ Perpustakaan.java
@@ -2,18 +2,15 @@ import java.util.Scanner;
 
 class Kategori {
     String nama;
-    
     public Kategori(String nama) {
         this.nama = nama;
     }
 }
 
 class Buku {
-    String judul;
+    String judul, penulis, sinopsis;
     int tahunTerbit;
     Kategori kategori;
-    String penulis;
-    String sinopsis;
     
     public Buku(String judul, int tahunTerbit, Kategori kategori, String penulis, String sinopsis) {
         this.judul = judul;
@@ -28,60 +25,43 @@ class Buku {
     }
     
     public double cekKesamaan(Buku bukuLain) {
-        int totalAttribute = 4; // judul, tahunTerbit, kategori, penulis
         int kesamaan = 0;
-        
-        if (this.judul.equalsIgnoreCase(bukuLain.judul)) kesamaan++;
-        if (this.tahunTerbit == bukuLain.tahunTerbit) kesamaan++;
-        if (this.kategori.nama.equalsIgnoreCase(bukuLain.kategori.nama)) kesamaan++;
-        if (this.penulis.equalsIgnoreCase(bukuLain.penulis)) kesamaan++;
-        
-        return (kesamaan / (double) totalAttribute) * 100;
+        if (judul.equalsIgnoreCase(bukuLain.judul)) kesamaan++;
+        if (tahunTerbit == bukuLain.tahunTerbit) kesamaan++;
+        if (kategori.nama.equalsIgnoreCase(bukuLain.kategori.nama)) kesamaan++;
+        if (penulis.equalsIgnoreCase(bukuLain.penulis)) kesamaan++;
+        if (sinopsis.equalsIgnoreCase(bukuLain.sinopsis)) kesamaan++;
+        return (kesamaan / 5.0) * 100;
     }
     
     public Buku copy() {
-        return new Buku(this.judul, this.tahunTerbit, this.kategori, this.penulis, this.sinopsis);
+        return new Buku(judul, tahunTerbit, kategori, penulis, sinopsis);
     }
 }
 
 public class Perpustakaan {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
         Kategori teknologi = new Kategori("Teknologi");
         Kategori filsafat = new Kategori("Filsafat");
         Kategori sejarah = new Kategori("Sejarah");
-        Kategori agama = new Kategori("Agama");
         Kategori psikologi = new Kategori("Psikologi");
-        Kategori politik = new Kategori("Politik");
         Kategori fiksi = new Kategori("Fiksi");
         
-        Buku[] bukuList = {
-            new Buku("Buku Pemrograman 1", 2001, teknologi, "Zulfikar Ramzy", "Buku ini membahas dasar-dasar pemrograman."),
-            new Buku("Buku Pemrograman 2", 2002, teknologi, "Zulfikar Ramzy", "Buku ini membahas algoritma tingkat lanjut."),
-            new Buku("Buku Filsafat 1", 2006, filsafat, "Zulfikar Ramzy", "Pemikiran filsafat klasik dan modern."),
-            new Buku("Buku Sejarah 1", 2011, sejarah, "Zulfikar Ramzy", "Sejarah dunia dari zaman kuno hingga modern."),
-            new Buku("Buku Agama 1", 2016, agama, "Zulfikar Ramzy", "Pembahasan mendalam mengenai nilai-nilai agama."),
-            new Buku("Buku Psikologi 1", 2021, psikologi, "Zulfikar Ramzy", "Psikologi perkembangan manusia."),
-            new Buku("Buku Politik 1", 2026, politik, "Zulfikar Ramzy", "Dinamika politik dalam era digital."),
-            new Buku("Buku Fiksi 1", 2031, fiksi, "Zulfikar Ramzy", "Sebuah kisah petualangan penuh makna.")
-        };
+        Buku buku1 = new Buku("Buku Pemrograman", 2022, teknologi, "Zulfikar Ramzy", "Buku ini membahas konsep pemrograman modern dan penerapannya dalam dunia kerja.");
+        Buku buku2 = new Buku("Buku Filsafat", 2023, filsafat, "Zulfikar Ramzy", "Buku ini membahas pemikiran filsafat dari berbagai era.");
+        Buku buku3 = new Buku("Buku Sejarah", 2024, sejarah, "Zulfikar Ramzy", "Buku ini menjelaskan sejarah peradaban dunia dengan rinci.");
+        Buku buku4 = new Buku("Buku Psikologi", 2025, psikologi, "Zulfikar Ramzy", "Buku ini mengupas berbagai teori psikologi dan penerapannya.");
+        Buku buku5 = new Buku("Buku Fiksi", 2026, fiksi, "Zulfikar Ramzy", "Buku ini adalah novel fiksi yang penuh dengan petualangan dan misteri.");
         
-        for (Buku buku : bukuList) {
+        Buku[] daftarBuku = {buku1, buku2, buku3, buku4, buku5};
+        
+        for (Buku buku : daftarBuku) {
             System.out.println("Judul: " + buku.judul);
-            System.out.println("Tahun: " + buku.tahunTerbit);
             System.out.println("Kategori: " + buku.kategori.nama);
             System.out.println("Penulis: " + buku.penulis);
             System.out.println("Sinopsis: " + buku.sinopsis);
             System.out.println("Jumlah Kata dalam Sinopsis: " + buku.hitungJumlahKata());
             System.out.println("--------------------");
         }
-        
-        Buku buku1 = bukuList[0];
-        Buku buku2 = bukuList[1];
-        System.out.println("\nTingkat Kesamaan Buku1 & Buku2: " + buku1.cekKesamaan(buku2) + "%");
-        
-        Buku bukuCopy = buku1.copy();
-        System.out.println("\nSalinan Buku: " + bukuCopy.judul + " - " + bukuCopy.penulis);
     }
 }
